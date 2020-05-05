6fa8b60e8dab762a2770f103496e803ee1047ae7
github username <github email address>
2020-05-04 19:23:08 -0700

    add github omniauth to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable, :validatable
+    devise :database_authenticatable, :registerable, :validatable, :omniauthable, :omniauth_providers => [:github]
 
-    def display_name
-        first_name + " " + last_name
+    def self.from_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.name = auth.info.name
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+      end      
     end
 end
