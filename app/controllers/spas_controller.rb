97434182b10001b8730bd3f2705363c729678dd5
github username <github email address>
2020-05-05 14:40:54 -0700

    add spa new/create routes

--- app/controllers/spas_controller.rb
+++ app/controllers/spas_controller.rb
@@ -1,5 +1,5 @@
 class SpasController < ApplicationController
-    before_action :set_spa, only: [:show]
+    before_action :set_spa, only: [:show, :edit, :update, :destroy]
     def index
         @spas = Spa.order(:name)
     end
@@ -7,9 +7,35 @@ class SpasController < ApplicationController
     def show
     end
 
+    def new
+        @spa = Spa.new
+    end
+
+    def create
+        @spa = Spa.create(spa_params)
+        if @spa.valid?
+            redirect_to spa_path(@spa)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
     private
 
     def set_spa
         @spa = Spa.find_by(id: params[:id])
+    end
+
+    def spa_params
+        params.require(:spa).permit(:name, :address1, :address2, :city, :state, :zip, :phone_number)
     end
 end
