3c88d06db9b031b902a24aea8f478e1ac3be8b96
github username <github email address>
2020-05-04 18:19:17 -0700

    setup devise and user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable, :validatable
+
+    def display_name
+        first_name + " " + last_name
+    end
+end
