bae9965796956cf24281fffde1ebb61f1bc26681
github username <github email address>
2020-05-05 14:16:12 -0700

    add spa show page and route

--- app/controllers/spas_controller.rb
+++ app/controllers/spas_controller.rb
@@ -1,5 +1,15 @@
 class SpasController < ApplicationController
+    before_action :set_spa, only: [:show]
     def index
         @spas = Spa.order(:name)
+    end
+
+    def show
+    end
+
+    private
+
+    def set_spa
+        @spa = Spa.find_by(id: params[:id])
     end
 end
