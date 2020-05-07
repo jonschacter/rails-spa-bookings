b127e6432610e49f396589fdf9b7e07a42766f3b
github username <github email address>
2020-05-06 19:09:57 -0700

    add treatment show page

--- app/models/spa.rb
+++ app/models/spa.rb
@@ -2,14 +2,4 @@ class Spa < ApplicationRecord
     has_many :technicians, :dependent => :delete_all
     has_many :treatments
     has_many :appointments, :through => :treatments
-    accepts_nested_attributes_for :technicians
-
-    def technicians_attributes=(attributes)
-        self.technicians.delete_all
-        attributes.each do |i, technician_hash|
-            if !technician_hash[:name].blank?
-                self.technicians.build(technician_hash)
-            end
-        end
-    end
 end
